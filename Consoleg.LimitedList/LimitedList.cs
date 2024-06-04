361dc8e0fc52441ab52058ee5f6e424858f02aad
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-06-04 13:56:48 +0200

    Implement Add in LimitedList

--- Consoleg.LimitedList/LimitedList.cs
+++ Consoleg.LimitedList/LimitedList.cs
@@ -1,14 +1,26 @@
 ﻿namespace Consoleg.LimitedList
 {
-    public class LimitedList<T>
+    public class LimitedList<T> 
     {
         private readonly int _capacity;
-        private List<T> list;
+        private List<T> _list;
+
+        public int Count => _list.Count;
+        public bool IsFull => _capacity <= Count;
 
         public LimitedList(int capacity)
         {
             _capacity = Math.Max(capacity, 2);
-            list = new List<T>(_capacity);
+            _list = new List<T>(_capacity);
+        }
+
+        public bool Add(T item)
+        {
+            ArgumentNullException.ThrowIfNull(item, "item");
+
+            if (IsFull) return false;
+            _list.Add(item); return true;
+
         }
     }
 }
